929fa97cbd5f3f0525ba4bd373baabf1ecdcdbbb
Sam Britton <dasclockwork@gmail.com>
2014-06-19 19:51:43 -0700

    Create a controller for the Dashboard

---
+++ src/DevTrw/LbBundle/DevTrwLbBundle/Controller/DashboardController.php
@@ -0,0 +1,13 @@
+<?php
+//
+//namespace DevTrw\LbBundle\DevTrwLbBundle\Controller;
+//
+//use Symfony\Component\HttpFoundation\Response;
+//
+//class DashboardController
+//    {
+//        public function indexAction($name)
+//        {
+//            return new Response('<html><body>Hello '.$name.'!</body></html>');
+//        }
+//    }
