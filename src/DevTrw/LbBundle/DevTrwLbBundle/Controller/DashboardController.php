4139b673cd014e854b64648123fd7164d15ad5b3
Sam Britton <dasclockwork@gmail.com>
2014-06-23 15:57:15 -0700

    First link to view

--- src/DevTrw/LbBundle/DevTrwLbBundle/Controller/DashboardController.php
+++ src/DevTrw/LbBundle/DevTrwLbBundle/Controller/DashboardController.php
@@ -1,13 +1,15 @@
 <?php
-//
-//namespace DevTrw\LbBundle\DevTrwLbBundle\Controller;
-//
-//use Symfony\Component\HttpFoundation\Response;
-//
-//class DashboardController
-//    {
-//        public function indexAction($name)
-//        {
-//            return new Response('<html><body>Hello '.$name.'!</body></html>');
-//        }
-//    }
+
+namespace DevTrw\LbBundle\DevTrwLbBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DashboardController extends Controller
+    {
+        public function indexAction()
+        {
+            return $this->render('DevTrwLbBundle:Dashboard:dashboard.html.twig');
+                        //return new Response('<html><body>Hello '.$name.'!</body></html>');
+        }
+    }
--- src/DevTrw/LbBundle/DevTrwLbBundle/Controller/DefaultController.php
+++ src/DevTrw/LbBundle/DevTrwLbBundle/Controller/DefaultController.php
@@ -1,13 +1,13 @@
 <?php
-
-namespace DevTrw\LbBundle\DevTrwLbBundle\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-class DefaultController extends Controller
-{
-    public function indexAction($name)
-    {
-        return $this->render('DevTrwLbBundle:Default:index.html.twig', array('name' => $name));
-    }
-}
+//
+//namespace DevTrw\LbBundle\DevTrwLbBundle\Controller;
+//
+//use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+//
+//class DefaultController extends Controller
+//{
+//    public function indexAction($name)
+//    {
+//        return $this->render('DevTrwLbBundle:Default:index.html.twig', array('name' => $name));
+//    }
+//}
