8672acef323a5e6aea4b65018b9763ddde0be558
Sam Britton <dasclockwork@gmail.com>
2014-06-23 18:53:40 -0700

    Create template

--- src/DevTrw/LbBundle/DevTrwLbBundle/Controller/DashboardController.php
+++ src/DevTrw/LbBundle/DevTrwLbBundle/Controller/DashboardController.php
@@ -2,7 +2,6 @@
 
 namespace DevTrw\LbBundle\DevTrwLbBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DashboardController extends Controller
@@ -10,6 +9,6 @@ class DashboardController extends Controller
         public function indexAction()
         {
             return $this->render('DevTrwLbBundle:Dashboard:dashboard.html.twig');
-                        //return new Response('<html><body>Hello '.$name.'!</body></html>');
+
         }
     }
